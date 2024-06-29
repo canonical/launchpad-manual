90608989d15cf2dbdf9a538a03517c03d87a3658
Jared Nielsen <nielsen.jared@gmail.com>
2024-06-29 03:54:27 -0400

    Fix 'JavascriptUnitTesting' link (#72)
    Co-authored-by: Jürgen Gmach <juergen.gmach@canonical.com>

--- custom_conf.py
+++ custom_conf.py
@@ -133,7 +133,6 @@ linkcheck_ignore = [
     'Database/TableRenamePatch',  # needs update
     'Debugging#Profiling%20page%20requests',  # needs update
     'Debugging#Special%20URLs',  # needs update
-    'JavascriptUnitTesting',  # needs update
     'JavascriptUnitTesting/MockIo',  # needs update
     'PolicyAndProcess/Accessibility',  # needs update
     'Running',  # needs update
