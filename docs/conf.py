76eff1a5f90771e442bd8442960da615e80b6630
Charles Odada <charles.odada@canonical.com>
2026-04-23 12:41:28 +0300

    Replace png tag with svg version.
    Signed-off-by: Charles Odada <charles.odada@canonical.com>

--- docs/conf.py
+++ docs/conf.py
@@ -110,7 +110,7 @@ html_context = {
     # Product tag image; the orange part of your logo, shown in the page header
     #
     # TODO: To add a tag image, uncomment and update as needed.
-    'product_tag': '_static/Launchpad-tag.png',
+    'product_tag': '_static/Tag-Launchpad-Orange-Digital.svg',
     # Your Discourse instance URL
     #
     # TODO: Change to your Discourse instance URL or leave empty.
@@ -127,6 +127,7 @@ html_context = {
     "matrix": "https://matrix.to/#/#launchpad:ubuntu.com",
     # Your documentation GitHub repository URL
     #
+    # Link to Launchpad's answer portal for users.
     "answers": "https://answers.launchpad.net/",
     #
     # TODO: Change to your documentation GitHub repository URL or leave empty.
